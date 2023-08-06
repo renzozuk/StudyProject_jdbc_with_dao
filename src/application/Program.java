617d51e9356081af35e24264ac2671489e255239
Renzo Zukeram <renzo.zuk19@gmail.com>
2023-08-06 18:35:00 -0300

    The methods findById and findByDepartment were implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,25 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Cláudio", "craudio@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("=== TEST 1: seller findById ===");
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
